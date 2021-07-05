010914d248c944e6ee0eb8f2a0e9e763055bb220
md319 <86971167+md319@users.noreply.github.com>
2021-07-05 21:27:23 +0530

    Add files via upload

---
+++ Final Assignment/src/data.js
@@ -0,0 +1,47 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Apple Air',
+      price: 1800,
+      image: 'https://picsum.photos/id/119/2000/1600',
+    },
+    {
+      id: '2',
+      name: 'Woman high heels',
+      price: 1000,
+      image: 'https://picsum.photos/id/21/2500/2008',
+    },
+    {
+      id: '3',
+      name: 'cactus plant',
+      price: 30,
+      image: 'https://picsum.photos/id/248/1200/1050',
+    },
+    {
+      id: '4',
+      name: 'vintage Car',
+      price: 40500,
+      image: 'https://picsum.photos/id/111/3500/2656',
+    },
+    {
+      id: '5',
+      name: 'classic mug',
+      price: 100,
+      image: 'https://picsum.photos/id//30/2500/1600',
+    },
+    {
+      id: '6',
+      name: 'Apple MacBook',
+      price: 1400,
+      image: 'https://picsum.photos/id/180/2000/1600',
+    },
+    {
+      id: '7',
+      name: 'Mini cooper car',
+      price: 10600,
+      image: 'https://picsum.photos/id/133/2000/1600',
+    },
+  ],
+};
+export default data;
